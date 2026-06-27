7a5cc6fbdf6f8ce2750de16612771d81851e959d
achyudaram <achyudaram74@gmail.com>
2026-06-27 00:26:57 -0700

    Add files via upload

---
+++ achyudaram task5.py
@@ -0,0 +1,235 @@
+#task 5
+#list tasks
+'''#1
+a=["achu","aravind","hem","siva","karthi"]
+print(a)
+#2
+print(a.append("jey"))
+print(a)
+#3
+print(a.pop())
+print(a)
+
+#4
+a=["achu","aravind","hem","siva","karthi"]
+for i in a:
+    print(i,end=" ")
+    print()
+
+#5
+a=[5,8,10,67,69]
+print("the largest number in list is:",a[4])
+print("the largest number in list is:",a[3])
+
+#6
+a=[5,8,10,67,69]
+count=0
+for i in a:
+    if i%2==0:
+        count+=1
+        print("the even numbers are:",i)
+for i in a:
+    if i%2==1:
+        count+=1
+        print("the odd numbers are:",i)
+
+#7
+a=[5,8,10,67,69]
+print(a.reverse())
+print(a)
+
+#8
+a=[10,8,5,67,69]
+print(a.sort())
+print(a)
+
+#9
+a=[5,8,10,67,69]
+sum=0
+for i in a:
+    sum+=i
+print("the sum of elements is:",sum)
+
+#10
+a=[5,8,10,67,69]
+print(a[3])'''
+
+#tuple tasks
+'''#1
+a=(5,8,10,67,69)
+#2
+print(a[0])
+print(a[1])
+print(a[2])
+print(a[3])
+print(a[4])
+#3
+a=(5,8,10,67,69)
+print(len(a))
+
+#4
+a=(5,8,10,67,67,67,69)
+print(a.count(67))
+
+#5
+a=(5,8,10,67,69)
+print(a.index(5))
+print(a.index(8))
+print(a.index(10))
+print(a.index(67))
+print(a.index(69))
+
+#6
+a=(5,8,10,67,69)
+print(a[0:4])
+
+#7
+a=(5,8,10,67,69)
+x=list(a)
+print(x)
+
+#8
+a=[5,8,10,67,69]
+x=tuple(a)
+print(x)
+
+#9
+a=(5,8,10,67,69)
+print("the maximum value is:",a[4])
+print("the minimum value is:",a[3])
+
+#10
+a=("achu","aravind","hem","siva","karthi")
+for name in a:
+    print(name)'''
+
+#set tasks
+#1
+'''a={5,8,10,67,69}
+print(a)
+#2
+a={5,8,10,67,69}
+print(a.add(4))
+print(a)
+#3
+a={5,8,10,67,69}
+print(a.remove(5))
+print(a)
+
+#4
+a={5,8,10,67,69}
+b={5,8,4,10,67,12,69}
+print(a|b)
+
+#5
+a={5,8,10,67,69}
+b={5,8,4,10,67,12,69}
+print(a&b)
+
+#6
+b={5,8,4,10,67,12,69}
+a={5,8,10,67,69}
+print(b^a)
+
+#7
+a=[5,8,10,67,67,67,69]
+x=set(a)
+print(x)
+y=list(x)
+print(y)
+
+#8
+a={5,8,10,67,69}
+b={5,8,4,10,67,12,69}
+print(a.issubset(b))
+print(b.issuperset(a))
+
+#10
+b=[5,8,4,10,67,12,69]
+x=set(b)
+print(x)
+
+#dictionarty tasks
+#1
+d={"name":"achu","marks":90}
+print(d)
+
+#2
+d={"name":"achu","marks":90}
+d["age"]=18
+print(d)
+
+#3
+d={"name":"achyudaram","marks":90}
+d.update({"name":"aravind","marks":67})
+print(d)
+
+#4
+d={"name":"aravind","marks":67}
+d.pop("name")
+print(d)
+
+#5
+d={"name":"aravind","marks":67}
+print(d.keys())
+print(d.values())'''
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
